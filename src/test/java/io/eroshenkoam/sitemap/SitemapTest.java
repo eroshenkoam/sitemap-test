3cf9aa4734117c37adf7e53251cd7892e3694cb9
Artem Eroshenko <eroshenkoam@me.com>
2018-09-24 14:38:30 +0300

    some updates

--- src/test/java/io/eroshenkoam/sitemap/SitemapTest.java
+++ src/test/java/io/eroshenkoam/sitemap/SitemapTest.java
@@ -1,24 +1,22 @@
 package io.eroshenkoam.sitemap;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.eroshenkoam.sitemap.dto.Sitemap;
 import io.eroshenkoam.sitemap.dto.Url;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.zip.GZIPInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
@@ -27,41 +25,27 @@ import static org.assertj.core.api.Assertions.fail;
 public class SitemapTest {
 
     private static final String SITEMAP_URL = "SITEMAP_URL";
+    private static final String MAX_URL_COUNT = "MAX_URL_COUNT";
 
     private final Map<String, String> unique = new HashMap<>();
 
     @Parameterized.Parameter(0)
-    public String file;
+    public String sitemap;
 
     @Parameterized.Parameter(1)
     public String url;
 
     @Parameterized.Parameters(name = "{0}: {1}")
     public static Collection<Object[]> parameters() {
-        final String[] files = new String[]{
-                "sitemap-offers-0.xml",
-                "sitemap-listing-0.xml",
-                "sitemap-listing-1.xml",
-                "sitemap-listing-2.xml",
-                "sitemap-listing-3.xml",
-                "sitemap-listing-4.xml",
-                "sitemap-listing-5.xml",
-                "sitemap-listing-6.xml",
-                "sitemap-listing-7.xml",
-                "sitemap-listing-8.xml",
-                "sitemap-listing-9.xml",
-                "sitemap-listing-10.xml",
-                "sitemap-listing-11.xml",
-                "sitemap-listing-12.xml",
-                "sitemap-listing-13.xml"
-        };
+        final String sitemapUrl = getSitemapUrl();
         final List<Object[]> parameters = new ArrayList<>();
-        Arrays.stream(files).forEach(file -> {
-            fromResource(file).forEach(url -> {
-                parameters.add(new Object[]{file, url.getLoc()});
+        readListValue(sitemapUrl, Sitemap.class).forEach(sitemap -> {
+            readListValue(sitemap.getLoc(), Url.class).forEach(url -> {
+                parameters.add(new Object[]{sitemap.getLoc(), url.getLoc()});
             });
         });
-        return parameters;
+        final Integer maxUrlCount = getMaxUrlCount();
+        return parameters.subList(0, maxUrlCount);
     }
 
     @Test
@@ -72,9 +56,9 @@ public class SitemapTest {
     @Test
     public void uniqueTest() {
         if (unique.containsKey(url)) {
-            fail(String.format("Url %s already contains in file: %s", url, file));
+            fail(String.format("Url %s already contains in file: %s", url, sitemap));
         } else {
-            unique.put(url, file);
+            unique.put(url, sitemap);
         }
     }
 
@@ -84,27 +68,28 @@ public class SitemapTest {
                 .hasFieldOrPropertyWithValue("responseCode", 200);
     }
 
-    private static String getResponseContent(String url ) throws Exception {
-        final URLConnection connection = new URL(url).openConnection();
-        byte[] bytes = IOUtils.toByteArray(connection);
-        return new String(bytes, Charset.forName("UTF-8"));
-    }
-
     private static String getSitemapUrl() {
         return Optional.ofNullable(System.getenv(SITEMAP_URL))
                 .orElse("https://realty.yandex.ru/sitemap.xml");
     }
 
-    private static List<Url> fromResource(final String name) {
-        final XmlMapper mapper = new XmlMapper();
-        try {
-            return mapper.readValue(
-                    ClassLoader.getSystemResourceAsStream(name),
-                    mapper.getTypeFactory().constructCollectionType(List.class, Url.class)
-            );
+    private static Integer getMaxUrlCount() {
+        return Optional.ofNullable(System.getenv(MAX_URL_COUNT))
+                .map(Integer::parseInt)
+                .orElse(1000);
+    }
+
+    private static <T> List<T> readListValue(final String url, Class<T> type) {
+        try (InputStream stream = new URL(url).openStream()) {
+            return readListValue(new GZIPInputStream(stream), type);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static <T> List<T> readListValue(final InputStream stream, Class<T> type) throws IOException {
+        final XmlMapper mapper = new XmlMapper();
+        return mapper.readValue(stream, mapper.getTypeFactory().constructCollectionType(List.class, type));
     }
 
 }
